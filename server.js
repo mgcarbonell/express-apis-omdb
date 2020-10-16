2daf1e2307b02ce4d36e6d69bf21f3892b024643
mgcarbonell <mgcarbonell@gmail.com>
2020-10-15 21:04:47 -0700

    Creating queries, getting OMDb Response

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios');
+const fs = require('fs');
 const app = express();
+const key = '9cf61e91'
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,8 +20,28 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
 });
+
+
+app.get('/results', function(req, res) {
+  const queryString = {
+  params: {
+    s: req.query.movie,
+    apiKey: key,
+  }
+
+};
+
+axios.get('http://omdbapi.com', queryString)
+        .then(function (omdbResponse) {
+          console.log(omdbResponse.data.Search);
+          res.render('results', {movies: omdbResponse.data.Search})
+        });
+});
+
+
+// app.get route for returning 1 value for returning 1 IMDb id movie/:id
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
